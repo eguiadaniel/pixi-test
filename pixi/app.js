1c48d889cf95dc3bbc49b331a811478e35ac649b
Daniel Eguia <eguiadaniel@gmail.com>
2021-08-08 12:26:48 +0200

    images added

--- pixi/app.js
+++ pixi/app.js
@@ -1,5 +1,7 @@
 let app;
 let player;
+let keys = {};
+let keysDiv;
 
 window.onload = function () {
   app = new PIXI.Application({
@@ -19,13 +21,57 @@ window.onload = function () {
   app.stage.addChild(player);
 
   //mouse interactions
-  app.stage.interactive = true;
-  app.stage.on('pointermove', movePlayer);
+  //   app.stage.interactive = true;
+  //   app.stage.on('pointermove', movePlayer);
+
+  //keyboard event handlers
+  window.addEventListener('keydown', keysDown);
+  window.addEventListener('keyup', keysUp);
+
+  app.ticker.add(gameLoop);
+
+  keysDiv = document.getElementById('keys');
+  //   keysDiv = document.querySelector('#keys');
 };
 
-function movePlayer(e) {
-  let pos = e.data.global;
-  player.x = pos.x;
-  player.y = pos.y;
-  //   console.log(e.data.global);
+// function movePlayer(e) {
+//   let pos = e.data.global;
+//   player.x = pos.x;
+//   player.y = pos.y;
+//   //   console.log(e.data.global);
+// }
+
+function keysDown(e) {
+  console.log(e.keyCode);
+  keys[e.keyCode] = true;
+}
+
+function keysUp(e) {
+  console.log(e.keyCode);
+  keys[e.keyCode] = false;
+}
+
+function gameLoop() {
+  //   keysDiv.innerHTML = JSON.stringify(keys);
+
+  let speed = 5;
+  // W
+  if (keys['38']) {
+    player.y -= speed;
+  }
+
+  // S
+  if (keys['40']) {
+    player.y += speed;
+  }
+
+  // A
+  if (keys['37']) {
+    player.x -= speed;
+  }
+
+  // D
+  if (keys['39']) {
+    player.x += speed;
+  }
 }
